aa42202b088bda96ce908266ac24211bf6ccbe00
Rujirojn <ZRujirojn.B@pttdigital.com>
2020-05-27 16:31:37 +0700

    fix

--- flutterframeassit/lib/main.dart
+++ flutterframeassit/lib/main.dart
@@ -10,7 +10,7 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {updat
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
